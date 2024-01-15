31a761d41f4c0f595e3e14234c26dda159fd4817
Moataz <chaabanemoataz@outlook.fr>
2024-01-15 22:38:02 +0100

    first commit

---
+++ src/webparts/blueButtons/components/IBlueButtonsProps.ts
@@ -0,0 +1,7 @@
+export interface IBlueButtonsProps {
+  description: string;
+  isDarkTheme: boolean;
+  environmentMessage: string;
+  hasTeamsContext: boolean;
+  userDisplayName: string;
+}
---
+++ src/webparts/blueButtons/loc/mystrings.d.ts
@@ -0,0 +1,19 @@
+declare interface IBlueButtonsWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+  UnknownEnvironment: string;
+}
+
+declare module 'BlueButtonsWebPartStrings' {
+  const strings: IBlueButtonsWebPartStrings;
+  export = strings;
+}
